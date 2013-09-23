25371a6c9a1af3168c8f554ee21829b6d95b6b4f
Nelson Rodrigues <nelson@fullfront.pt>
2013-09-23 23:04:28 +0100

    Passed more tests

--- lib/rgba-png.rb
+++ lib/rgba-png.rb
@@ -2,10 +2,7 @@ require "oily_png"
 
 class RgbaPng
 
-  @colors = []
-  @rgba = /rgba\((\d+),(\d+),(\d+),(\d?.\d+)\)/
-
-  def self.parse(file, destination)
+  def self.init(file, destination)
     if !file || !destination
       puts 'Error: wrong number of arguments, expected 2'
       puts 'Usage: rgba-png sourcefile destination'
@@ -13,40 +10,50 @@ class RgbaPng
       return
     end
 
-    destination = destination + '/'
+    colors = self.parse(file)
+    create_files colors, destination
+  end
+
+  def self.parse(file)
+    colors = []
+    rgba = /rgba\((\d+),(\d+),(\d+),(\d?.\d+)\)/
 
     IO.foreach(file) do |line|
-      is_rgba = line.match @rgba
+      is_rgba = line.match rgba
 
       if is_rgba
         color = {
             r: is_rgba[1].to_i,
             g: is_rgba[2].to_i,
             b: is_rgba[3].to_i,
-            a: (is_rgba[4].to_f * 255).to_i # Convert alpha value to 0-255 range
+            a: (is_rgba[4].to_f * 255).round(1).to_i # Convert alpha value to 0-255 range
         }
 
-        @colors.push(color)
+        colors.push(color)
       end
     end
 
-    create_files @colors, destination
-
-    puts @colors.uniq.length.to_s + ' file(s) created!'
+    colors
   end
 
-  # Create file name from the color, ex: 255-255-255-0_8
-  def self.color_name(color)
-    # Convert the alpha back to the 0-1 range and substitute the '.' for a '_'
-    alpha = (color[:a] / 255.to_f).round(1).to_s.sub('.', '_')
-    color[:r].to_s + '-' + color[:g].to_s + '-' + color[:b].to_s + '-' + alpha
+  # Create file name from the color, ex: 255-255-255-0.8
+  def self.file_name(color)
+    # Convert the alpha to 0-1 range
+    alpha = (color[:a] / 255.to_f).round(1)
+
+    color[:r].to_s + '-' + color[:g].to_s + '-' + color[:b].to_s + '-' + alpha.to_s
   end
 
   def self.create_files(colors, destination)
     colors.uniq.each do |color|
       bg = ChunkyPNG::Color.rgba(color[:r], color[:g], color[:b], color[:a])
       png = ChunkyPNG::Image.new(16, 16, bg)
-      png.save(destination + (self.color_name(color) + '.png'), interlace: true)
+
+      unless Dir.exists? destination
+        Dir.mkdir destination
+      end
+
+      png.save(destination + '/' + (self.file_name(color) + '.png'), interlace: true)
     end
   end
 
--- test/test_rgba-png.rb
+++ test/test_rgba-png.rb
@@ -1,5 +1,6 @@
 require 'test/unit'
 require 'rgba-png'
+require 'fileutils'
 
 if ENV['RUBYMINE']
   require 'minitest/reporters'
@@ -9,22 +10,52 @@ end
 class RgbaPngTest < Test::Unit::TestCase
 
   def teardown
-    destination = File.dirname(__FILE__) + '/images/'
-    File.delete(destination + '255-200-100-0_5.png')
+    new_folder = File.dirname(__FILE__) + '/i-dont-exist-yet'
+    path = File.dirname(__FILE__) + '/images/'
+    file = '255-200-100-0.5.png'
+
+    if File.exists?(path + file)
+      File.delete(path + file)
+    end
+
+    if Dir.exists?(new_folder)
+      FileUtils.rm_r new_folder
+    end
   end
 
-  def test_create_file_name_from_color_hash
-    color = {r: 255, g: 200, b: 100, a:128}
-    assert_equal "255-200-100-0_5", RgbaPng.color_name(color)
+  def test_create_destination_folder_if_not_exists
+    colors = [{r: 255, g: 200, b: 100, a:128}]
+    destination = File.dirname(__FILE__) + '/i-dont-exist-yet'
+
+    RgbaPng.create_files(colors, destination)
+
+    assert_equal true, Dir.exists?(destination)
   end
 
   def test_create_file_from_color_hash_array
     colors = [{r: 255, g: 200, b: 100, a:128}]
-    destination = File.dirname(__FILE__) + '/images/'
+    destination = File.dirname(__FILE__) + '/images'
 
     RgbaPng.create_files(colors, destination)
 
-    assert_equal true, File.exists?(destination + '255-200-100-0_5.png')
+    assert_equal true, File.exists?(destination + '/255-200-100-0.5.png')
+  end
+
+  def test_create_file_name_from_color_hash
+    color = {r: 255, g: 200, b: 100, a:128}
+    assert_equal "255-200-100-0.5", RgbaPng.file_name(color)
+  end
+
+  def test_parse_source_file_should_create_hash_array
+    source_file = File.dirname(__FILE__) + '/test-style.css'
+
+    expected = [
+        {r: 255, g: 200, b: 254, a: 76},
+        {r: 0, g: 200, b: 70, a: 127},
+        {r: 255, g: 120, b: 10, a: 204}
+    ]
+
+    assert_equal expected, RgbaPng.parse(source_file)
   end
 
 end
